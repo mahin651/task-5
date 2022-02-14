26b15c746981c8d9b6343aabd7b85a3574825e73
Your Name <mahinmittal2018@gmail.com>
2022-02-14 16:52:48 +0530

    added cource controller,route,model

---
+++ routes/cources.js
@@ -0,0 +1,9 @@
+const router =require("express").Router();
+
+router.post("/");
+router.get("/");
+router.get("/courceID");
+router.put("/courceID");
+router.delete("/courceID");
+
+module.exports=router;
